0e68418836bfd9ca170eff28b16236168e09adf8
K.V.S Mohith <5starmohith@gmail.com>
2025-06-14 20:05:11 +0530

    NEW DESIGNS ARE ADDED BROCHURE

--- designs.js
+++ designs.js
@@ -29,6 +29,8 @@ document.addEventListener("DOMContentLoaded", () => {
         { name: "AAVISHKAR SEASON-4 E-POSTER EVENT POSTER", category: "fest", img: "assests/AAVISHKAR 4 E-POSTER POSTER.png", desc: "AAVISHKAR SEASON-4 EPOSTER EVENT POSTER", designer: "5Star Designs", link: "#", social: "https://www.instagram.com/p/DHGCwIUhFF2/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
         { name: "AAVISHKAR SEASON-4 TECHNICAL QUIZ EVENT POSTER", category: "fest", img: "assests/AAVISHKAR 4 TECHNICAL QUIZ POSTER.png", desc: "AAVISHKAR SEASON-4 TECHNICAL QUIZ EVENT POSTER", designer: "5Star Designs", link: "#", social: "https://www.instagram.com/p/DHGCd10hm8E/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
         { name: "AAVISHKAR SEASON-4 ESPORTS EVENT POSTER", category: "fest", img: "assests/AAVISHKAR 4 ESPORTS POSTER.png", desc: "AAVISHKAR SEASON-4 E-SPORTS EVENT POSTER", designer: "5Star Designs", link: "#", social: "https://www.instagram.com/p/DHGCwIUhFF2/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
+        { name: "AAVISHKAR SEASON-4 BROCHURE PART 1", category: "fest", img: "./assests/1.png", desc: "AAVISHKAR SEASON-4 CLEAN AND ATTRACTIVE BROCHURE", designer: "5Star Designs", link: "#", social: "https://www.instagram.com/p/DHz9S6-tmIV/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
+        { name: "AAVISHKAR SEASON-4 BROCHURE PART 2", category: "fest", img: "./assests/2.png", desc: "AAVISHKAR SEASON-4 CLEAN AND ATTRACTIVE BROCHURE", designer: "5Star Designs", link: "#", social: "https://www.instagram.com/p/DHz9S6-tmIV/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
    ];
     
     console.log("Designs loaded:", designs);
